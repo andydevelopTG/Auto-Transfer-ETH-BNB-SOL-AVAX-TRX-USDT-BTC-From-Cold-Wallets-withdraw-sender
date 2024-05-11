878a0916f369b4dda120982041f733aa83c36d33
Andy <93540642+andydevelopTG@users.noreply.github.com>
2024-05-11 16:27:11 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,3 @@
+If You Want Script Autosender ETH/BNB/BSC And Other Coins/ Tokens 1 Block Withdraw , OR you want  Autosender Stopper/Remover   Contact: IF YOU WANT BOUGHT DM ME IN TELEGRAM 👇 👇 👇
+https://t.me/andy_develop
+https://t.me/andy_develop
